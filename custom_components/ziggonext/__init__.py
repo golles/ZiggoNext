6ea0bdb3cdd9b355c61aad07f46f0a6387c9b702
Rudolf Offereins <r.offereins@gmail.com>
2020-01-18 16:17:32 +0100

    Countrycode (#14)
    * Updated ziggonext to version 0.5.5
    * Added support for country codes
    
    * Rename countryCode to country_code to match style guidelines.

--- custom_components/ziggonext/__init__.py
+++ custom_components/ziggonext/__init__.py
@@ -1,6 +1,9 @@
 """The ziggo_mediabox_next component."""
 import logging
-from .const import ZIGGO_API
+from .const import (
+    ZIGGO_API,
+    CONF_COUNTRY_CODE
+)
 from homeassistant.const import (
     CONF_USERNAME,
     CONF_PASSWORD
@@ -15,14 +18,15 @@ _LOGGER = logging.getLogger(__name__)
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
         vol.Required(CONF_USERNAME): cv.string,
-        vol.Required(CONF_PASSWORD): cv.string
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_COUNTRY_CODE, default="nl"): cv.string
     })
 }, extra=vol.ALLOW_EXTRA,)
 
 def setup(hass, config):
     """Your controller/hub specific code."""
     # Data that you want to share with your platforms
-    api = ZiggoNext(config[DOMAIN][CONF_USERNAME], config[DOMAIN][CONF_PASSWORD])
+    api = ZiggoNext(config[DOMAIN][CONF_USERNAME], config[DOMAIN][CONF_PASSWORD],config[DOMAIN][CONF_COUNTRY_CODE])
     api.initialize(_LOGGER)
     hass.data[ZIGGO_API] = api
     hass.helpers.discovery.load_platform('media_player', DOMAIN, {}, config)
--- custom_components/ziggonext/const.py
+++ custom_components/ziggonext/const.py
@@ -1,2 +1,3 @@
 """Constants for the Ziggo Mediabox Next integration."""
 ZIGGO_API = "ziggo_api"
+CONF_COUNTRY_CODE = "country_code"
